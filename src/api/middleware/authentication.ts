285a28e7968cefa8b1cd5cff3921d213e73cd493
jubissi <71803301+bissiju@users.noreply.github.com>
2024-03-22 14:12:57 -0300

    correcao de erros build

--- src/api/middleware/authentication.ts
+++ src/api/middleware/authentication.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
-import Authenticatior from "~gateways/authentication/cognito";
-import { UserType } from "~domain/repository/authenticationRepository";
+import Authenticatior from "./gateways/authentication/cognito";
+import { UserType } from "./domain/repository/authenticationRepository";
 
 
 export default function authenticate(type: UserType) {
